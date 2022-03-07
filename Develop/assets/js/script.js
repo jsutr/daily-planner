a377b36d0888d6b2b7544e8d968ee5457f690e6f
justinsuter-qiKFlV <suter765@gmail.com>
2022-03-06 22:50:07 -0800

    variables and attributes

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,23 @@
+// Variables
+
+var saveBtn = $(".saveBtn");
+var currentHour = moment().format("HH")
+var currentHourInterger = parseInt(currentHour);
+
+
+// Attributes 
+
+$("#8").attr("data-time"), moment("8:00", "H:mm").format("HH");
+$("#9").attr("data-time", moment("9:00", "H:mm").format("HH"));
+$("#10").attr("data-time", moment("10:00", "HH:mm").format("HH"));
+$("#11").attr("data-time", moment("11:00", "HH:mm").format("HH"));
+$("#12").attr("data-time", moment("12:00", "HH:mm").format("HH"));
+$("#13").attr("data-time", moment("13:00", "HH:mm").format("HH"));
+$("#14").attr("data-time", moment("14:00", "HH:mm").format("HH"));
+$("#15").attr("data-time", moment("15:00", "HH:mm").format("HH"));
+$("#16").attr("data-time", moment("16:00", "HH:mm").format("HH"));
+$("#17").attr("data-time", moment("17:00", "HH:mm").format("HH"));
+
+
+// jQuery
+
